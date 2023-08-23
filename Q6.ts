8f34a2bc29ed4843d9aab174522ff19bcafbabd2
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 13:51:04 -0700

    Q6

---
+++ Q6.ts
@@ -0,0 +1,16 @@
+//Q6 - Write a program that takes temperature and check it. 
+//If it is cold then suggest the user to wear warm clothes 
+//and so on according to the weather.
+var temperature:number=15
+if (temperature<18)
+{
+    console.log("its cold weather suggest wear warm clothes")    
+} 
+else if (temperature>18 && temperature<33) 
+{
+    console.log("its normal weather suggest wear causal clothes")    
+} 
+else 
+{
+    console.log("its warm weather duggest wear lightweight clothes")   
+}

c57663c13243190b1690acdf7be66efde2c04b36
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 13:26:15 -0700

    Q5

---
+++ Q5.ts
@@ -0,0 +1,14 @@
+/*Q5 - Create a program that determines the category of a user-provided age. 
+If the age is between 0 and 12, print "Child." If
+ it's between 13 and 19, print "Teenager." Otherwise, print "Adult.*/
+ var age:number=12
+ if (age<=12) 
+ {
+    console.log("this person is child")   
+ } else if (age>12 && age<=19) 
+{
+    console.log("this person is teenager")   
+ } else 
+ {
+   console.log("adult")  
+ }

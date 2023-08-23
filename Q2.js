0bc1e52402193d3d0054980e838694be58bf7464
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 12:21:43 -0700

    Q2

---
+++ Q2.js
@@ -0,0 +1,6 @@
+"use strict";
+//Q2 - Write a program that calculates the percentage
+var totalMarks = 555;
+var obtainMarks = 490;
+var calculatePersentage = obtainMarks / totalMarks * 100;
+console.log("Persentage of marks is =", calculatePersentage);
---
+++ Q2.ts
@@ -0,0 +1,5 @@
+//Q2 - Write a program that calculates the percentage
+var totalMarks:number=555 
+var obtainMarks:number=490
+var calculatePersentage:number=obtainMarks/totalMarks*100
+console.log("Persentage of marks is =",calculatePersentage)

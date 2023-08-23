92354a26053c57d4fd98fbede68865af1669dfea
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 12:05:44 -0700

    Q1

---
+++ Q1.js
@@ -0,0 +1,6 @@
+"use strict";
+/// Q1 - Write a program to convert the temperature 
+// from Celsius to Fahrenheit and vice verse.
+var num1 = 36;
+var temperature = num1 * (9 / 5) + 32;
+console.log("temp in fahrenheit is =", temperature);
---
+++ Q1.ts
@@ -0,0 +1,5 @@
+/// Q1 - Write a program to convert the temperature 
+// from Celsius to Fahrenheit and vice verse.
+var num1:number=36
+var temperature:number=num1*(9/5)+32
+console.log("temp in fahrenheit is =",temperature)

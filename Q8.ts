680c05cdd53ec82c3bc8af579c33eefec09ac899
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 21:25:09 -0700

    Q8

---
+++ Q8.ts
@@ -0,0 +1,9 @@
+//Q8 - Write a program that checks if the given year is leap year or not.
+var year:number=2016
+if (year%4==0)
+{
+    console.log("this year is leap year")
+} else {
+    
+    console.log("this year is not leap year")
+}

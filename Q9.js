ad8c771d891f8f3a5d2743a406fc579ea1ba0e50
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 21:37:34 -0700

    Q9

---
+++ Q9.js
@@ -0,0 +1,28 @@
+"use strict";
+// Q9 - Develop a program that determines the day of the week.
+//  Ask the user for a number (1-7) and use nested if
+//   statements to print the corresponding day's name.
+var daysOfWeek = 5;
+if (daysOfWeek == 1) {
+    console.log("Monday");
+}
+else {
+    if (daysOfWeek == 2) {
+        console.log("Tuseday");
+    }
+    else if (daysOfWeek == 3) {
+        console.log("Wednesday");
+    }
+    else if (daysOfWeek == 4) {
+        console.log("Thursday");
+    }
+    else if (daysOfWeek == 5) {
+        console.log("Friday");
+    }
+    else if (daysOfWeek == 6) {
+        console.log("Saturday");
+    }
+    else {
+        console.log("Sunday");
+    }
+}

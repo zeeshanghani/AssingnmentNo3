41c361d08ca2f1510820dd4ff353ee3f41c45236
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-23 22:02:36 -0700

    Q10

---
+++ Q10.js
@@ -0,0 +1,18 @@
+"use strict";
+// Q10 - Write a program that takes the number of units consumed by a user 
+// if it is greater than 100 then add 10% tax 
+// if greater than 200 then add 15% of tax so on up to 
+// if greater than 500 then add 25% of tax
+// Where the tax amount will be calculated by the amount of bill.\
+var unitRate = 55;
+var consumUnit = 210;
+var bill = unitRate * consumUnit;
+if (consumUnit > 100) {
+    console.log("Consum Unit above the 100 add 10% tax", bill + (bill * (10 / 100)));
+}
+else if (consumUnit > 200 && consumUnit < 500) {
+    console.log("Consum Unit above the 200 add 15% tax", bill + (bill * (15 / 100)));
+}
+else {
+    console.log("Consum Unit above the 500 add 25% tax", bill = (bill * (25 / 100)));
+}
